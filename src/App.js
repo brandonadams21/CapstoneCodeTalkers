6e9943920eac9ddd0c87710b852f5d61cf972c07
Brandon Adams <brandonadams@Brandons-MBP.attlocal.net>
2022-03-30 16:23:31 -0500

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,66 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
-import Home from "./Home/Home";
-import "./index.scss";
+// import Navbar from "./components/Navbar/Navbar";
+// import Home from "./Home/Home";
+// import "./index.scss";
 
-export default class App extends React.Component {
-  constructor(props) {
-    this.state = {
-      user: props.user,
-      isLogedIn: false,
-    };
-  }
-  userIsLogedIn() {}
-  componentDidMount() {
-    if (this.state.user.username && this.state.user.password) {
-      fetch("http://localhost:", {
-        method: "GET",
-        credentials: "include",
-        referrerPolicy: "origin-when-cross-origin",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          Accept: "application/json",
-        },
-      })
-        .then((resp) => resp.json())
-        .then((user) => {
-          if (user.username.length) this.setState({ user: user });
-        });
-    }
-  }
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <Home />
-      </div>
-    );
-  }
+// export default class App extends React.Component {
+  // constructor(props) {
+  //   this.state = {
+  //     user: props.user,
+  //     isLogedIn: false,
+  //   };
+  // }
+  // userIsLogedIn() {}
+  // componentDidMount() {
+  //   if (this.state.user.username && this.state.user.password) {
+  //     fetch("http://localhost:", {
+  //       method: "GET",
+  //       credentials: "include",
+  //       referrerPolicy: "origin-when-cross-origin",
+  //       headers: {
+  //         "Content-Type": "application/x-www-form-urlencoded",
+  //         Accept: "application/json",
+  //       },
+  //     })
+  //       .then((resp) => resp.json())
+  //       .then((user) => {
+  //         if (user.username.length) this.setState({ user: user });
+  //       });
+  //   }
+  // }
+  // render() {
+//     function App() {
+//     return (
+//       <div className="App">
+//         <Navbar />
+//         <Home />
+//       </div>
+//     );
+//   }
+
+// export default App
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
